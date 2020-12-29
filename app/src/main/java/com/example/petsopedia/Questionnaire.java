7f06dc429b73606cfe503030a90f0ea2324656aa
CaitlinMooney <75536966+CaitlinMooney@users.noreply.github.com>
2020-12-29 12:04:28 +0000

    Small updates to Questionnaire, working on Spinner switches but struggling just now - leave commented out.

--- app/src/main/java/com/example/petsopedia/Questionnaire.java
+++ app/src/main/java/com/example/petsopedia/Questionnaire.java
@@ -2,69 +2,76 @@ package com.example.petsopedia;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.List;
+
 
 public class Questionnaire extends AppCompatActivity {
-
-    Button pet_info;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.questionnaire);
 
-        Spinner ageSpinner = (Spinner) findViewById(R.id.age_spinner);
         Spinner typeSpinner = (Spinner) findViewById(R.id.which_spinner);
         Spinner breedSpinner = (Spinner) findViewById(R.id.breed_spinner);
-        Spinner weightSpinner = (Spinner) findViewById(R.id.weight_spinner);
-        Spinner activitySpinner = (Spinner) findViewById(R.id.activity_spinner);
 
-// Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> ageAdapter = ArrayAdapter.createFromResource(this,
-                R.array.pet_age, android.R.layout.simple_spinner_item);
+// ~~~ create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> typeAdapter = ArrayAdapter.createFromResource(this,
                 R.array.pet_type, android.R.layout.simple_spinner_item);
-        ArrayAdapter<CharSequence> breedAdapter = ArrayAdapter.createFromResource(this,
-                R.array.pet_breed, android.R.layout.simple_spinner_item);
-        ArrayAdapter<CharSequence> weightAdapter = ArrayAdapter.createFromResource(this,
-                R.array.pet_weight, android.R.layout.simple_spinner_item);
-        ArrayAdapter<CharSequence> activityAdapter = ArrayAdapter.createFromResource(this,
-                R.array.pet_activity, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> dogBreedAdapter = ArrayAdapter.createFromResource(this,
+                R.array.dog_breeds, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> catBreedAdapter = ArrayAdapter.createFromResource(this,
+                R.array.cat_breeds, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> rabbitBreedAdapter = ArrayAdapter.createFromResource(this,
+                R.array.rabbit_breeds, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> hamsterBreedAdapter = ArrayAdapter.createFromResource(this,
+                R.array.hamster_breeds, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> birdBreedAdapter = ArrayAdapter.createFromResource(this,
+                R.array.bird_breeds, android.R.layout.simple_spinner_item);
 
-// Specify the layout to use when the list of choices appears
-        ageAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// ~~~ specify the layout to use when the list of choices appears
         typeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        breedAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        weightAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        activityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dogBreedAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        catBreedAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        rabbitBreedAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        hamsterBreedAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        birdBreedAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
 // ~~~ apply the adapter to the spinner
-        ageSpinner.setAdapter(ageAdapter);
         typeSpinner.setAdapter(typeAdapter);
-        breedSpinner.setAdapter(breedAdapter);
-        weightSpinner.setAdapter(weightAdapter);
-        activitySpinner.setAdapter(activityAdapter);
 
-        // ~~~ button for moving from first page to next page
+        // ~~~ delete after fixing switch statement
+        breedSpinner.setAdapter(dogBreedAdapter);
 
-//        pet_info = (Button)findViewById(R.id.main_button);
+// ~~~ this currently doesn't work - do not delete! leave commented out please.
+
+//        typeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
 //
-//        pet_info.setOnClickListener(new View.OnClickListener() {
-//
-//            public void onClick(View v)
-//            {
-//
-//                Intent intent = new Intent(Questionnaire.this, PetInfo.class);
-//
-//                startActivity(intent);
+//            if(typeSpinner.getSelectedItem().equals("Dogs")) {
+//                breedSpinner.setAdapter(dogBreedAdapter);
+//            } else if(typeSpinner.getSelectedItem().equals("Cats")) {
+//                breedSpinner.setAdapter(catBreedAdapter);
+//            } else if (typeSpinner.getSelectedItem().equals("Rabbits")) {
+//                breedSpinner.setAdapter(rabbitBreedAdapter);
 //            }
+//
+//            }
+//
+//            public void onNothingSelected(AdapterView<?> arg0) {
+//
+//            }
+//
 //        });
+        }
 
-    }
 
 }
 
