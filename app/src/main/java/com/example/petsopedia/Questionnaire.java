c638c7729d00f6bf86a2d5a2ddd4d6a09dc33b06
CaitlinMooney <75536966+CaitlinMooney@users.noreply.github.com>
2020-12-29 18:50:21 +0000

    Switch statement within Questionnaire now working.

--- app/src/main/java/com/example/petsopedia/Questionnaire.java
+++ app/src/main/java/com/example/petsopedia/Questionnaire.java
@@ -18,58 +18,79 @@ public class Questionnaire extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.questionnaire);
 
+       // String[] typeList = {"Dogs", "Rabbits","Cats"};
+       String[] dogBreeds = {"Alsatian", "Border Collie","Labrador"};
+       String[] catBreeds = {"Bombay", "British Shorthair","Persian"};
+       String[] rabbitBreeds = {"Dutch", "English Lop","Flemish"};
+       String[] hamsterBreeds = {"Chinese", "Russian","Syrian"};
+       String[] birdBreeds = {"Parrot", "Parakeet","Cockatiel"};
+
         Spinner typeSpinner = (Spinner) findViewById(R.id.which_spinner);
         Spinner breedSpinner = (Spinner) findViewById(R.id.breed_spinner);
 
 // ~~~ create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> typeAdapter = ArrayAdapter.createFromResource(this,
                 R.array.pet_type, android.R.layout.simple_spinner_item);
-        ArrayAdapter<CharSequence> dogBreedAdapter = ArrayAdapter.createFromResource(this,
-                R.array.dog_breeds, android.R.layout.simple_spinner_item);
-        ArrayAdapter<CharSequence> catBreedAdapter = ArrayAdapter.createFromResource(this,
-                R.array.cat_breeds, android.R.layout.simple_spinner_item);
-        ArrayAdapter<CharSequence> rabbitBreedAdapter = ArrayAdapter.createFromResource(this,
-                R.array.rabbit_breeds, android.R.layout.simple_spinner_item);
-        ArrayAdapter<CharSequence> hamsterBreedAdapter = ArrayAdapter.createFromResource(this,
-                R.array.hamster_breeds, android.R.layout.simple_spinner_item);
-        ArrayAdapter<CharSequence> birdBreedAdapter = ArrayAdapter.createFromResource(this,
-                R.array.bird_breeds, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> dogBreedAdapter = new ArrayAdapter<CharSequence>(this,
+               android.R.layout.simple_spinner_item, dogBreeds);
+        ArrayAdapter<CharSequence> catBreedAdapter = new ArrayAdapter<CharSequence>(this,
+               android.R.layout.simple_spinner_item, catBreeds);
+        ArrayAdapter<CharSequence> rabbitBreedAdapter = new ArrayAdapter<CharSequence>(this,
+                android.R.layout.simple_spinner_item, rabbitBreeds);
+        ArrayAdapter<CharSequence> hamsterBreedAdapter = new ArrayAdapter<CharSequence>(this,
+                android.R.layout.simple_spinner_item, hamsterBreeds);
+        ArrayAdapter<CharSequence> birdBreedAdapter = new ArrayAdapter<CharSequence>(this,
+                android.R.layout.simple_spinner_item, birdBreeds);
 
 // ~~~ specify the layout to use when the list of choices appears
-        typeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        dogBreedAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        catBreedAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        rabbitBreedAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        hamsterBreedAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        birdBreedAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+         typeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
 // ~~~ apply the adapter to the spinner
         typeSpinner.setAdapter(typeAdapter);
 
-        // ~~~ delete after fixing switch statement
-        breedSpinner.setAdapter(dogBreedAdapter);
+        typeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
 
-// ~~~ this currently doesn't work - do not delete! leave commented out please.
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
 
-//        typeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-//            @Override
-//            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-//
-//            if(typeSpinner.getSelectedItem().equals("Dogs")) {
-//                breedSpinner.setAdapter(dogBreedAdapter);
-//            } else if(typeSpinner.getSelectedItem().equals("Cats")) {
-//                breedSpinner.setAdapter(catBreedAdapter);
-//            } else if (typeSpinner.getSelectedItem().equals("Rabbits")) {
-//                breedSpinner.setAdapter(rabbitBreedAdapter);
-//            }
-//
-//            }
-//
-//            public void onNothingSelected(AdapterView<?> arg0) {
-//
-//            }
-//
-//        });
+                switch (typeSpinner.getSelectedItem().toString()) {
+                    case "Dog":
+                        dogBreedAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        breedSpinner.setAdapter(dogBreedAdapter);
+                        break;
+
+                    case "Cat":
+                        catBreedAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        breedSpinner.setAdapter(catBreedAdapter);
+                        break;
+
+                    case "Rabbit":
+                        rabbitBreedAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        breedSpinner.setAdapter(rabbitBreedAdapter);
+                        break;
+
+                    case "Hamster":
+                        hamsterBreedAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        breedSpinner.setAdapter(hamsterBreedAdapter);
+                        break;
+
+                    case "Bird":
+                        birdBreedAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        breedSpinner.setAdapter(birdBreedAdapter);
+                        break;
+
+                    default:
+                        breedSpinner.setEnabled(false);
+                        break;
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
         }
 
 
